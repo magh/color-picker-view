5fcb9de50148423ccc1eea0ca96138a9905e95f6
Mathias <mathias@x1c>
2013-11-23 14:42:48 +0100

    Cleanup. Reverted changes to original from http://color-picker-view.googlecode.com/svn/trunk/

--- library/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/david/git/color-picker-view-read-only/library/bin/AndroidManifest.xml : \
-/home/david/git/color-picker-view-read-only/library/AndroidManifest.xml \
--- library/src/afzkl/development/colorpickerview/dialog/ColorPickerDialog.java
+++ library/src/afzkl/development/colorpickerview/dialog/ColorPickerDialog.java
@@ -28,93 +28,92 @@ import android.view.View;
 import android.widget.LinearLayout;
 
 public class ColorPickerDialog extends AlertDialog implements
-	ColorPickerView.OnColorChangedListener {
+		ColorPickerView.OnColorChangedListener {
 
-    private ColorPickerView mColorPicker;
+	private ColorPickerView mColorPicker;
 
-    private ColorPanelView mOldColor;
-    private ColorPanelView mNewColor;
+	private ColorPanelView mOldColor;
+	private ColorPanelView mNewColor;
 
-    private OnColorChangedListener mListener;
+	private OnColorChangedListener mListener;
 
-    public ColorPickerDialog(Context context, int initialColor) {
-	this(context, initialColor, null);
+	public ColorPickerDialog(Context context, int initialColor) {
+		this(context, initialColor, null);
 
-	init(initialColor);
-    }
-
-    public ColorPickerDialog(Context context, int initialColor,
-	    OnColorChangedListener listener) {
-	super(context);
-	mListener = listener;
-	init(initialColor);
-    }
-
-    private void init(int color) {
-	// To fight color branding.
-	getWindow().setFormat(PixelFormat.RGBA_8888);
-	setUp(color);
-    }
-
-    private void setUp(int color) {
-	boolean isLandscapeLayout = false;
-
-	LayoutInflater inflater = (LayoutInflater) getContext()
-		.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-	View layout = inflater.inflate(R.layout.dialog_color_picker, null);
-
-	setView(layout);
-
-	setTitle(getContext().getString(R.string.color_dialog_title));
-	// setIcon(android.R.drawable.ic_dialog_info);
-
-	LinearLayout landscapeLayout = (LinearLayout) layout
-		.findViewById(R.id.dialog_color_picker_extra_layout_landscape);
-
-	if (landscapeLayout != null) {
-	    isLandscapeLayout = true;
+		init(initialColor);
+	}
+	
+	public ColorPickerDialog(Context context, int initialColor, OnColorChangedListener listener) {
+		super(context);
+		mListener = listener;
+		init(initialColor);
 	}
 
-	mColorPicker = (ColorPickerView) layout
-		.findViewById(R.id.color_picker_view);
-	mOldColor = (ColorPanelView) layout.findViewById(R.id.color_panel_old);
-	mNewColor = (ColorPanelView) layout.findViewById(R.id.color_panel_new);
-
-	if (!isLandscapeLayout) {
-	    ((LinearLayout) mOldColor.getParent()).setPadding(
-		    Math.round(mColorPicker.getDrawingOffset()), 0,
-		    Math.round(mColorPicker.getDrawingOffset()), 0);
-
-	} else {
-	    landscapeLayout.setPadding(0, 0,
-		    Math.round(mColorPicker.getDrawingOffset()), 0);
-	    setTitle(null);
+	private void init(int color) {
+		// To fight color branding.
+		getWindow().setFormat(PixelFormat.RGBA_8888);
+		setUp(color);
 	}
 
-	mColorPicker.setOnColorChangedListener(this);
+	private void setUp(int color) {
+		boolean isLandscapeLayout = false;
+		
+		LayoutInflater inflater = (LayoutInflater) getContext()
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		View layout = inflater.inflate(R.layout.dialog_color_picker, null);
 
-	mOldColor.setColor(color);
-	mColorPicker.setColor(color, true);
+		setView(layout);
 
-    }
+		setTitle("Pick a Color");
+		// setIcon(android.R.drawable.ic_dialog_info);
+			
+		LinearLayout landscapeLayout = (LinearLayout) layout.findViewById(R.id.dialog_color_picker_extra_layout_landscape);
+		
+		if(landscapeLayout != null) {
+			isLandscapeLayout = true;
+		}
+		
+				
+		mColorPicker = (ColorPickerView) layout
+				.findViewById(R.id.color_picker_view);
+		mOldColor = (ColorPanelView) layout.findViewById(R.id.color_panel_old);
+		mNewColor = (ColorPanelView) layout.findViewById(R.id.color_panel_new);
 
-    @Override
-    public void onColorChanged(int color) {
-	mNewColor.setColor(color);
+		if(!isLandscapeLayout) {
+			((LinearLayout) mOldColor.getParent()).setPadding(Math
+					.round(mColorPicker.getDrawingOffset()), 0, Math
+					.round(mColorPicker.getDrawingOffset()), 0);
+			
+		}
+		else {
+			landscapeLayout.setPadding(0, 0, Math.round(mColorPicker.getDrawingOffset()), 0);
+			setTitle(null);
+		}
 
-	if (mListener != null) {
-	    mListener.onColorChanged(color);
+		mColorPicker.setOnColorChangedListener(this);
+
+		mOldColor.setColor(color);
+		mColorPicker.setColor(color, true);
 
 	}
 
-    }
+	@Override
+	public void onColorChanged(int color) {
+		mNewColor.setColor(color);
 
-    public void setAlphaSliderVisible(boolean visible) {
-	mColorPicker.setAlphaSliderVisible(visible);
-    }
+		if (mListener != null) {
+			mListener.onColorChanged(color);
+			
+		}
 
-    public int getColor() {
-	return mColorPicker.getColor();
-    }
+	}
+
+	public void setAlphaSliderVisible(boolean visible) {
+		mColorPicker.setAlphaSliderVisible(visible);
+	}
+
+	public int getColor() {
+		return mColorPicker.getColor();
+	}
 
 }
